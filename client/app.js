63788a9f30c0a0fef5e90b527bc47722b3ff38d6
Joanna Stachowicz <joannastachowicz19@gmail.com>
2021-01-30 20:06:16 +0100

    Add new functionalities

--- client/app.js
+++ client/app.js
@@ -5,6 +5,11 @@ const addMessageForm = document.getElementById('add-messages-form');
 const userNameInput = document.getElementById('username');
 const messageContentInput = document.getElementById('message-content');
 
+const socket = io();
+socket.on('message', ({ author, content }) => addMessage(author, content));
+socket.on('newUser', ({ name }) => addMessage('Chat Bot', name + ' has joined the conversation!'));
+socket.on('removeUser', ({ name }) => addMessage('Chat Bot', name + ' has left the conversation... :('));
+
 let userName = '';
 
 const login = (event) => {
@@ -15,6 +20,7 @@ const login = (event) => {
     userName = userNameInput.value;
     loginForm.classList.remove('show');
     messagesSection.classList.add('show');
+    socket.emit('join', { name: userName })
   }
 };
 
@@ -39,6 +45,9 @@ const addMessage = (author, content) => {
 
   const div = document.createElement('div');
   div.classList.add('message__content');
+  if (author === 'Chat Bot') {
+    div.classList.add('message__skewed')
+  }
   div.innerHTML = content;
 
   message.insertAdjacentElement('beforeend', header);
@@ -48,10 +57,13 @@ const addMessage = (author, content) => {
 
 const sendMessage = (event) => {
   event.preventDefault();
-  if (messageContentInput.value === '') {
-    alert('Message cannot be empty.');
+
+  let messageContent = messageContentInput.value;
+  if (!messageContent.length) {
+    alert('You have to type something!');
   } else {
-    addMessage(userName, messageContentInput.value);
+    addMessage(userName, messageContent);
+    socket.emit('message', { author: userName, content: messageContent })
     messageContentInput.value = '';
   }
 };
--- server.js
+++ server.js
@@ -1,17 +1,43 @@
 const express = require('express');
 const path = require('path');
-// const hbs = require('express-handlebars');
+const socket = require('socket.io');
 
 const app = express();
+const server = app.listen(8000, () => {
+  console.log('Server is running on Port:', 8000)
+});
+const io = socket(server);
 
 app.use(express.static(path.join(__dirname, '/client')));
 
-// app.get('/', (req, res) => {
-//   res.render('index');
-// });
-
 let messages = [];
+let users = [];
 
-app.listen(8000, () => {
-  console.log('Server is running on port: 8000');
+io.on('connection', (socket) => {
+  console.log('New client! Its id – ' + socket.id);
+
+  socket.on('message', (message) => {
+    console.log('Oh, I\'ve got something from ' + socket.id);
+    messages.push(message);
+    socket.broadcast.emit('message', message);
+  });
+
+  socket.on('join', (join) => {
+    console.log('User ' + join.name + ' has joined. Its id is ' + socket.id);
+    join.id = socket.id;
+    users.push(join);
+    socket.broadcast.emit('newUser', join);
+  });
+
+  socket.on('disconnect', () => {
+    for (let i = 0; i < users.length; i++) {
+      if (users[i].id == socket.id) {
+        users.splice(i, 1);
+        socket.broadcast.emit('removeUser', users[i]);
+        break;
+      }
+    }
+    console.log('Oh, socket ' + socket.id + ' has left')
+  });
+  console.log('I\'ve added a listener on message and disconnect events \n');
 });
